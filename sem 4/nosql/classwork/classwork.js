a6c50593c1bedecd6a398c8f859bc24b22f1dc02
HarshaKy <harshaky6573@gmail.com>
2019-02-12 14:08:46 +0530

    asdf

--- sem 4/nosql/classwork/classwork.js
+++ sem 4/nosql/classwork/classwork.js
@@ -41,4 +41,72 @@ db.student.update({
 	}
 })
 
-// slice
+// create a collection books
+// insert -  id, username
+// embedded doc - title, author, pages
+
+db.createCollection("books")
+
+db.books.insert({
+	_id: 1,
+	name: "harsha ky",
+	info: [{
+		title: "book1",
+		author: "asdf",
+		pages: 123
+	}, {
+		title: "book2",
+		author: "qwer",
+		pages: 452
+	}]
+})
+
+db.books.insert({
+	_id: 2,
+	name: "Ronaldo",
+	info: [{
+		title: "harry potter part 1",
+		author: "JK Rowling",
+		pages: 566
+	}, {
+		title: "LOTR",
+		author: "JRR Tolkien",
+		pages: 1000
+	}]
+})
+
+db.books.insert({
+	_id: 3,
+	name: "Messi",
+	info: [{
+		title: "harry potter part 7",
+		author: "JK Rowling",
+		pages: 542
+	}, {
+		title: "Running out of book names",
+		author: "some author",
+		pages: 567
+	}, {
+		title: "Some good book",
+		author: "Cliche author",
+		pages: 654
+	}]
+})
+
+db.books.insert({
+	_id: 4,
+	name: "Ozil",
+	info: [{
+		title: "asdfqwer",
+		author: "not jk rowling",
+		pages: 542
+	}, {
+		title: "Running out of book names pt 2",
+		author: "some author again",
+		pages: 567
+	}, {
+		title: "Some good book pt 111",
+		author: "Cliche author gg",
+		pages: 654
+	}]
+})
