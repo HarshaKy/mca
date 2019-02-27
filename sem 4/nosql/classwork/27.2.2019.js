393eb6b7addf5d8e9df4ae470b49b5cc2f141028
HarshaKy <harshaky6573@gmail.com>
2019-02-27 10:04:56 +0530

    switch case

--- sem 4/nosql/classwork/27.2.2019.js
+++ sem 4/nosql/classwork/27.2.2019.js
@@ -18,3 +18,58 @@ db.student.aggregate([
     $out: "grades"
   }
 ])
+
+//
+db.sales.aggregate([
+  {
+    $group: {
+      _id: ""
+    }
+  }
+])
+
+//
+db.student.aggregate([
+  {
+    $project: {
+      name: 1,
+      summary: {
+        $switch: {
+          branches: [
+            {
+              case: {
+                $gte: [{$avg: "$grades"}, 90]
+              },
+              then: "great"
+            },
+
+            {
+              case: {
+                $and: [{
+                  $gte: [{
+                    $avg: "$grades"
+                  }, 80]
+                }, {
+                  $lt: [{
+                    $avg: "$grades"
+                  }, 90]
+                }]
+              },
+              then: "pretty well"
+            },
+
+            {
+              case: {
+                $lt: [{
+                  $avg: "$grades"
+                }, 80]
+              },
+              then: "can do  better"
+            }
+          ],
+          default: "no scores found"
+        }
+      }
+    }
+  }
+])
