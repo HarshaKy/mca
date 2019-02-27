52ec7dccca371f05e7590d34f078054b3de96642
HarshaKy <harshaky6573@gmail.com>
2019-02-27 09:10:12 +0530

    db commands

--- sem 4/nosql/classwork/27.2.2019.js
+++ sem 4/nosql/classwork/27.2.2019.js
@@ -1,1 +1,20 @@
 // filter items array to include documents that have price greater thaor equal to 100
+
+// failed students
+db.student.aggregate([
+  {
+    $project: {
+      name: 1,
+      grades: {
+        $filter: {
+          input: "$grades", as: "score",
+          cond: {
+            $lt: ["$$grades", 70]
+          }
+        }
+      }
+    }
+  }, {
+    $out: "grades"
+  }
+])
