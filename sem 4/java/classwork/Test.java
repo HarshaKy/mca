1751d53a6d2435b5a0b240a4127c9b0b5ca409c6
HarshaKy <harshaky6573@gmail.com>
2019-03-02 14:22:52 +0530

    more stuff

--- sem 4/java/classwork/Test.java
+++ sem 4/java/classwork/Test.java
@@ -1,18 +1,27 @@
 class Parent {
+  int m;
+
   Parent(int a) {
     System.out.println("Parent constructor.");
+    this.m = a;
+    System.out.println(this.m);
   }
 }
 
 class Child extends Parent {
+  int x;
+
   Child() {
     // System.out.println("Child constructor");
-    this(10);
+    this(10, 20);
+    // super();
     System.out.println("child 0 args constructor");
   }
 
-  Child(int a) {
-    System.out.println("child 1 argument constructor");
+  Child(int a, int b) {
+    super(b);
+    System.out.println("child 2 argument constructor");
+    this.x = b;
   }
 }
 
@@ -24,4 +33,4 @@ public class Test { // public class must have same name as .java file
 }
 
 // if user is defining constructor inside the class, there wont be a default
-// constructor created 
+// constructor created
