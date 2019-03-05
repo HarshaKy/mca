c667f7b6974807cc844eba891915901ff085b87e
HarshaKy <harshaky6573@gmail.com>
2019-03-05 09:25:23 +0530

    asdf

--- sem 4/nosql/classwork/march-5-2019.js
+++ sem 4/nosql/classwork/march-5-2019.js
@@ -14,3 +14,37 @@ db.products.aggregate([
     }
   }
 ])
+
+// some collection
+db.createCollection('mycol')
+
+//
+db.mycol.insert([
+  {
+    _id: 0,
+    a: 8
+  },
+  {
+    _id: 1,
+    a: [1,2,3,4,5,6]
+  },
+  {
+    _id: 2,
+    a: {
+      a: "apple",
+      b: "ball",
+      c: "cat"
+    }
+  },
+  {
+    _id: 3,
+    a: "asdf"
+  },
+  {
+    _id: 4,
+    a: NumberLong(71)
+  },
+  {
+    _id: 5
+  }
+])
