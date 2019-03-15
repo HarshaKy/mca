c0efc012099ff0e0d89ca38eb84f65ef09a00b4a
HarshaKy <harshaky6573@gmail.com>
2019-03-15 11:47:05 +0530

    java class over

--- sem4/java/classwork/mar-15.java
+++ sem4/java/classwork/mar-15.java
@@ -8,18 +8,36 @@
 // 	}
 // }
 
-interface asdf { // interfaces cant be final. interfaces also can't have constructors.
-	int x = 10; 
-	public void show();
+// interface asdf { // interfaces cant be final. interfaces also can't have constructors.
+// 	int x = 10; 
+// 	public void show();
+// 	// public void asdf(); // no constructors allowed m8.
+// }
+
+// class child implements asdf {
+// 	public void show() {
+// 		System.out.println(x);
+// 	}
+
+// 	public static void main(String[] args) {
+// 		child c = new child();
+// 		c.show();	
+// 	}
+// }
+
+interface intf1 {
+	public void get();
+	public void put();
 }
 
-class child implements asdf {
-	public void show() {
-		System.out.println(x);
-	}
+class child implements intf1 {
+	public void get () {}
+	public void put () {} // child should implement all methods in the parent interface.
 
 	public static void main(String[] args) {
 		child c = new child();
-		c.show();	
+		c.get();
 	}
 }
+
+// even interfaces are made into a .class file. 
