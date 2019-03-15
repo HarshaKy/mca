c71996ae8c41b398a39b9725c6ee50a4acb746dd
HarshaKy <harshaky6573@gmail.com>
2019-03-15 11:29:11 +0530

    learnin'

--- sem4/java/classwork/mar-15.java
+++ sem4/java/classwork/mar-15.java
@@ -1,9 +1,25 @@
-interface asdf {
-    int x = 1, y = 2;
+// interface asdf {
+// 	int x = 1, y = 2;
+// }
+
+// class child implements asdf {
+// 	public static void main(String[] args) {
+// 		System.out.println(x + " " + y); // variables have to be initialized in the interface for this to work.
+// 	}
+// }
+
+interface asdf { // interfaces cant be final. interfaces also can't have constructors.
+	int x = 10; 
+	public void show();
 }
 
 class child implements asdf {
-    public static void main(String[] args) {
-        System.out.println(x + " " + y); // variables have to be initialized in the interface for this to work.
-    }
+	public void show() {
+		System.out.println(x);
+	}
+
+	public static void main(String[] args) {
+		child c = new child();
+		c.show();	
+	}
 }
