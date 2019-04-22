7154f7b7b2da22015c297eff0e65cd917d80fdd6
HarshaKy <harshaky6573@gmail.com>
2019-04-22 14:15:49 +0530

    pymongo insert

--- sem4/nosql/classwork/pymongo-class.py
+++ sem4/nosql/classwork/pymongo-class.py
@@ -2,16 +2,28 @@ import pymongo
 from pymongo import MongoClient
 
 con = pymongo.MongoClient("mongodb://localhost")
-# con = MongoClient
-database = con.pes 
-coll = database.mca
 
-for i in range(10):
-    coll.insert_one({"name": "ABC"})
+db = con.pes 
+coll = db.employee
 
-docs = coll.find()
+def insert() :
+	empid = input("enter id")
+	empname = input("enter name")
 
-for i in docs:
-    print(i)
-# myColl = db.testdb
-# print("asdf")
+	db.emp.insert_one(
+		{
+			"id": empid,
+			"name": empname
+		}
+	)
+
+for i in range(5) :
+	insert()
+
+# for i in range(10):
+# 	coll.insert_one({"name": "ABC"})
+
+# docs = coll.find()
+
+# for i in docs:
+# 	print(i)
