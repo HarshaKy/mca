220a879594b0608702e88d0b09414732b2bdadf2
HarshaKy <harshaky6573@gmail.com>
2019-10-05 12:12:45 +0530

    ajp lab 7 part 1

---
+++ sem5/ajp/lab/eclipse-workspace/lab7/src/mcaSem5/SessionInfo.java
@@ -0,0 +1,64 @@
+package mcaSem5;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionInfo
+ */
+//@WebServlet("/SessionInfo")
+public class SessionInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionInfo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try {
+			 response.setContentType("text/html; charset=UTF-8");
+		     PrintWriter out = response.getWriter();
+		     
+		     out.println("<!DOCTYPE html>");
+		     out.println("<html><head>");
+		     out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+		     out.println("<title>Session Details</title></head>");
+		     out.println("<body><h1>Session Details</h1>");
+		     
+		     HttpSession session = request.getSession(false);
+		     String un = (String)session.getAttribute("userName");
+		     String p = (String)session.getAttribute("password");
+		     
+		     out.println("Hello " + un);
+		     out.println("Your Password is " + p);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+	}
+
+}
